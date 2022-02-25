11577e31c111a06557a4b19016614a7866268f1a
AAA3A-AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2022-02-25 11:00:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="AAA3A-utils",
+    name="AAA3A_utils",
     version="1.0.0",
     author="AAA3A",
     author_email=None,
     description="Utils for AAA3A-cogs.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/AAA3A-AAA3A/AAA3A-utils",
+    url="https://github.com/AAA3A-AAA3A/AAA3A_utils",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
