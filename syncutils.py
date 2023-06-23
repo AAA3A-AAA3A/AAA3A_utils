2a9bff3ccb80535945b6689e9ef1c0a6cc0040c0
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2023-06-23 11:34:17 +0200

    Bump utils version.

--- syncutils.py
+++ syncutils.py
@@ -11,7 +11,7 @@ import git
 from git import Repo
 
 # git -C %USERPROFILE%\Documents\GitHub\AAA3A_utils rev-list HEAD --count AAA3A_utils
-VERSION = 4.20
+VERSION = 5.0
 
 if VERSION is None:
     utils_repo_clone_location = Path(
